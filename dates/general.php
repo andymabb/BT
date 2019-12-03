04bd3843560b016f68554a9d0d35c054fd658ae1
Andy Mabbutt <andy@pontbren.com>
2019-12-03 14:22:31 +0000

    general dated added

--- dates/general.php
+++ dates/general.php
@@ -1,26 +1,82 @@
 <h3>General 2019/20</h3>
-<p>Details to follow</p>
-<!-- <table class="table table-responsive">
-	<tbody>
-		<tr><td>11/2 - 21/2</td>	<td>Year 11 Mock Examinations</td></tr>
-		<tr><td>20/2</td>			<td>PSE morning</td></tr>
-		<tr><td>21/2</td>			<td>School Eisteddfod</td></tr>
-		<tr><td>4/3 - 8/3</td>		<td>Year 10 Mock Examinations</td></tr>
-		<tr><td>06/3</td>			<td>7 a side competition in Llandovery</td></tr>
-		<tr><td>07/3</td>			<td>World Book Day</td></tr>
-		<tr><td>08/3</td>			<td>7 a side competition Berkhampstead</td></tr>
-		<tr><td>15/3</td>			<td>7 a side competition Barry</td></tr>
-		<tr><td>27/3</td>			<td>Vaccinations (Year 9)</td></tr>
-		<tr><td>01/4 - 5/4</td>		<td>Year 9 Classroom tests</td></tr>
-		<tr><td>10/4</td>			<td>UCAS Fair</td></tr>
-		<tr><td>29/04 - 10/5</td>	<td>National Tests</td></tr>
-		<tr><td>13/5-17/5</td>		<td>Year 7 and 8 Classroom tests</td></tr>
-		<tr><td>15/5</td>			<td>County Athletics</td></tr>
-		<tr><td>21/6</td>			<td>PSE morning</td></tr>
-		<tr><td>28/6</td>			<td>School Sports day</td></tr>
-		<tr><td>03/7</td>			<td>URDD Athletics</td></tr>
-		<tr><td>10/7</td>			<td>PSE morning</td></tr>
-		<tr><td>15/7</td>			<td>Bastille Day</td></tr>
-	</tbody>
-</table> -->
- 
+<!-- <p>Details to follow</p> -->
+<dl>
+	<dt>22/11/2019</dt>
+	<dd>URDD Swimming Gala</dd>
+	<dt>3-5/12/2019</dt>
+	<dd>Grease Musical Show</dd>
+	<dt>11/12/2019</dt>
+	<dd>Technocamps</dd>
+	<dt>16/12/2019</dt>
+	<dd>School house competition</dd>
+	<dt>18/12/2019</dt>
+	<dd>Awards Ceremony</dd>
+	<dt>13/01/2020</dt>
+	<dd>Art Workshop</dd>
+	<dt>17/01/2020</dt>
+	<dd>Technocamps</dd>
+	<dt>20/01/2020</dt>
+	<dd>Art Workshop</dd>
+	<dt>23/01/2020</dt>
+	<dd>PSE</dd>
+	<dt>24/01/2020</dt>
+	<dd>Chinese New Year</dd>
+	<dt>24/01/2020</dt>
+	<dd>Santes Dwynwen</dd>
+	<dt>27/01/2020</dt>
+	<dd>Holocaust remembrance day</dd>
+	<dt>27/1 - 28/1</dt>
+	<dd>HPV Vaccinations</dd>
+	<dt>07/02/2020</dt>
+	<dd>Welsh Music day</dd>
+	<dt>11/02/2020</dt>
+	<dd>Online Safety day</dd>
+	<dt>13/02/2020</dt>
+	<dd>School Eisteddfod</dd>
+	<dt>26/02/2020</dt>
+	<dd>D & T Workshop</dd>
+	<dt>02/03/2020</dt>
+	<dd>Chairing Ceremony</dd>
+	<dt>04/03/2020</dt>
+	<dd>7-a-side Rugby competition in Llandovery</dd>
+	<dt>05/03/2020</dt>
+	<dd>World Book Day</dd>
+	<dt>06/03/2020</dt>
+	<dd>BBC School Report</dd>
+	<dt>16/03/2020</dt>
+	<dd>Year 11 and 13 Pictures</dd>
+	<dt>27/03/2020</dt>
+	<dd>DTP Vaccinations</dd>
+	<dt>30/03/2020</dt>
+	<dd>School house competition</dd>
+	<dt>01/04/2020</dt>
+	<dd>Year 12 UCAS Fair</dd>
+	<dt>01/04/2020</dt>
+	<dd>Urdd 7 a side</dd>
+	<dt>24/04/2020</dt>
+	<dd>Year 12 UCAS Fair</dd>
+	<dt>06/05/2020</dt>
+	<dd>County Athletics</dd>
+	<dt>07/05/2020</dt>
+	<dd>Hamlet Lectures (Year 13)</dd>
+	<dt>12/06/2020</dt>
+	<dd>Geography Field Work</dd>
+	<dt>15/6 - 17/6</dt>
+	<dd>Art Workshop</dd>
+	<dt>17/06/2020</dt>
+	<dd>County Athletics</dd>
+	<dt>22/06/2020</dt>
+	<dd>Geography Field Work</dd>
+	<dt>22/6 - 26/6</dt>
+	<dd>'Dringo'n Uwch' week</dd>
+	<dt>23/06/2020</dt>
+	<dd>Primary Proms</dd>
+	<dt>30/06/2020</dt>
+	<dd>School Sports' Day</dd>
+	<dt>03/07/2020</dt>
+	<dd>URDD Athletics</dd>
+	<dt>06/07 - 10/7</dt>
+	<dd>Bridging week</dd>
+	<dt>14/07/2020</dt>
+	<dd>School house competition</dd>
+</dl>
--- dates/general_cy.php
+++ dates/general_cy.php
@@ -1,27 +1,82 @@
 <h3> Cyffredinol 2019/20</h3>
-<p>Manylion i ddilyn</p>
-<!-- To be confirmed -->
-
-<!-- <table class="table table-responsive">
-	<tbody>
-		<tr><td>11/2 - 21/2</td>	<td>Arholiadau mewnol bl11</td></tr>
-		<tr><td>20/2</td>			<td>Bore ABCH</td></tr>
-		<tr><td>21/2</td>			<td>Eisteddfod Ysgol</td></tr>
-		<tr><td>4/3 - 8/3</td>		<td>Arholiadau mewnol blwyddyn 10</td></tr>
-		<tr><td>06/3</td>			<td>Cystadleuaeth rygbi 7 bob ochr Llanymddyfri</td></tr>
-		<tr><td>07/3</td>			<td>Diwrnod y Llyfr</td></tr>
-		<tr><td>08/3</td>			<td>Taith rygbi 7 bob ochr i Berkhampstead</td></tr>
-		<tr><td>15/3</td>			<td>Taith rygbi 7 bob ochr i Bari</td></tr>
-		<tr><td>27/3</td>			<td>Pigiadau (Blwyddyn 9)</td></tr>
-		<tr><td>01/4 - 5/4</td>		<td>Profion dosbarth blwyddyn 9</td></tr>
-		<tr><td>10/4</td>			<td>Ffair UCAS</td></tr>
-		<tr><td>29/04 - 10/5</td>	<td>Profion Cenedlaethol</td></tr>
-		<tr><td>13/5-17/5</td>		<td>Profion Dosbarth blwyddyn 7 ac 8</td></tr>
-		<tr><td>15/5</td>			<td>Athletau'r SIR</td></tr>
-		<tr><td>21/6</td>			<td>Bore ABCh</td></tr>
-		<tr><td>28/6</td>			<td>Mabolgampau</td></tr>
-		<tr><td>03/7</td>			<td>Athletau'r URDD</td></tr>
-		<tr><td>10/7</td>			<td>Bore ABCH</td></tr>
-		<tr><td>15/7</td>			<td>Diwrnod Cenedlaethol Ffrainc</td></tr>
-	</tbody>
-</table> -->
+<!-- <p>Manylion i ddilyn</p> -->
+<dl>
+	<dt>22/11/2019</dt>
+	<dd>Gala Nofio'r URDD</dd>
+	<dt>3-5/12/2019</dt>
+	<dd>Sioe Gerdd Grîs</dd>
+	<dt>11/12/2019</dt>
+	<dd>Technocamps</dd>
+	<dt>16/12/2019</dt>
+	<dd>Rhynglysol</dd>
+	<dt>18/12/2019</dt>
+	<dd>Noson Wobrwyo</dd>
+	<dt>13/01/2020</dt>
+	<dd>Gweithdy Celf</dd>
+	<dt>17/01/2020</dt>
+	<dd>Technocamps</dd>
+	<dt>20/01/2020</dt>
+	<dd>Gweithdy Celf</dd>
+	<dt>23/01/2020</dt>
+	<dd>ABCh</dd>
+	<dt>24/01/2020</dt>
+	<dd>Blwyddyn Newydd Tseiniaidd</dd>
+	<dt>24/01/2020</dt>
+	<dd>Diwrnod Santes Dwynwen</dd>
+	<dt>27/01/2020</dt>
+	<dd>Diwrnod Cofio'r Holocost</dd>
+	<dt>27/1 - 28/1</dt>
+	<dd>Pigiadau HPV</dd>
+	<dt>07/02/2020</dt>
+	<dd>Dydd Miwsig Cymru</dd>
+	<dt>11/02/2020</dt>
+	<dd>Diwrnod Diogelwch ar y we</dd>
+	<dt>13/02/2020</dt>
+	<dd>Eisteddfod yr Ysgol</dd>
+	<dt>26/02/2020</dt>
+	<dd>Gweithdy DT</dd>
+	<dt>02/03/2020</dt>
+	<dd>Seremoni Cadeirio</dd>
+	<dt>04/03/2020</dt>
+	<dd>Cystadleuaeth Rygbi 7-bob-ochr Llanymddyfri</dd>
+	<dt>05/03/2020</dt>
+	<dd>Diwrnod y Llyfr</dd>
+	<dt>06/03/2020</dt>
+	<dd>BBC Adroddiad Ysgol</dd>
+	<dt>16/03/2020</dt>
+	<dd>Lluniau blwyddyn 11 a 13</dd>
+	<dt>27/03/2020</dt>
+	<dd>Pigiadau DTP</dd>
+	<dt>30/03/2020</dt>
+	<dd>Rhynglysol</dd>
+	<dt>01/04/2020</dt>
+	<dd>Ffair UCAS Blwyddyn 12</dd>
+	<dt>01/04/2020</dt>
+	<dd>Urdd 7 pob ochr</dd>
+	<dt>24/04/2020</dt>
+	<dd>Ffair UCAS Blwyddyn 12</dd>
+	<dt>06/05/2020</dt>
+	<dd>Athletau'r Sir</dd>
+	<dt>07/05/2020</dt>
+	<dd>Darlith Blwyddyn 13 Hamlet</dd>
+	<dt>12/06/2020</dt>
+	<dd>Gwaith maes daearyddiaeth</dd>
+	<dt>15/6 - 17/6</dt>
+	<dd>Gweithdy Celf</dd>
+	<dt>17/06/2020</dt>
+	<dd>Athletau'r Sir</dd>
+	<dt>22/06/2020</dt>
+	<dd>Gwaith maes daearyddiaeth</dd>
+	<dt>22/6 - 26/6</dt>
+	<dd>Wythnos Dringo'n Uwch</dd>
+	<dt>23/06/2020</dt>
+	<dd>Proms Cynradd</dd>
+	<dt>30/06/2020</dt>
+	<dd>Mabolgampau</dd>
+	<dt>03/07/2020</dt>
+	<dd>Athletau'r URDD</dd>
+	<dt>06/07 - 10/7</dt>
+	<dd>Wythnos Pontio</dd>
+	<dt>14/07/2020</dt>
+	<dd>Rhynglysol</dd>
+</dl>
