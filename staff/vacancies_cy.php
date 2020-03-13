e5bab7b123307fc77d7066327f0bbdf0fd9a146b
Andy Mabbutt <andy@pontbren.com>
2020-03-13 16:40:52 +0000

    Update vacancies_cy.php

--- staff/vacancies_cy.php
+++ staff/vacancies_cy.php
@@ -1,75 +1,68 @@
 <h2>Swyddi Gwag</h2>
 <!-- Dim Swyddi Gwag no vacancies  -->
 
-<h3>Athro Dyniaethau</h3>
-<p>Mae corff Llywodraethu yr ysgol am benodi Athro Dyniaethau. Rydym yn chwilio am berson sydd â hanes o ardderchogrwydd o ran addysgu, yn gallu ysbrydoli disgyblion a chydweithwyr, yn uchelgeisiol ac yn barod i ddatblygu strategaethau blaengar i gyfrannu at y Maes Dysgu a Phrofiad llwyddiannus iawn yma. Bydd disgwyl i’r ymgeisydd llwyddiannus i addysgu Dyniaethau a chyfrannu at wersi Hanes, Daearyddiaeth ac Addysg Grefyddol yn ôl arbenigedd yng NghA3, gan gyfrannu hefyd at gyrsiau TGAU a Safon Uwch.</p>
-<p>Ers agor yn 2003, mae Bryn Tawe wedi ennill bri fel ysgol gartrefol, lwyddiannus ac uchelgeisiol ar gyfer eu holl ddisgyblion. Rydym wedi ein lleoli ar gyrion Dinas Abertawe, yn gyfleus iawn ar gyfer yr M4. Mae staff gweithgar a thalentog yr ysgol yn allweddol wrth yrru llwyddiannau’r ysgol yn ogystal â sicrhau’r ymdeimlad cryf o gymuned sydd yn bodoli yma.</p>
-<p>Gyda datblygiadau cyffrous iawn ym myd addysg, â’r ffocws ar ddatblygu medrau llythrenned, rhifedd, digidol a meddwl dwfn disgyblion, bydd gan yr ymgeisydd llwyddiannus y cyfle i ysgogi, i ysbrydoli ac i arfogi ein disgyblion â’r medrau hanfodol ar gyfer dyfodol llwyddiannus.</p>
+<h3>Athro Parhaol Celf</h3>
+
+<p>Rydym yn chwilio am athro/athrawes ysbrydoledig ac uchelgeisiol i ymuno ag adran lwyddiannus iawn yn yr ysgol. Bydd disgwyl i’r ymgeisydd llwyddiannus i addysgu Celf ar draws bob cyfnod allweddol, gan gyfrannau at gyrsiau poblogaidd TGAU a Safon Uwch. Yn ogystal, bydd angen cyfrannu at addysgu CA3 yn yr adran Dylunio a Thechnolega bydd y gallu i gynnig Ffotograffiaeth at Safon Uwch yn fanteisiol iawn.</p> 
+
+<p>Gyda datblygiadau cyffrous iawn ym myd addysg, â’r ffocws ar ddatblygu medrau gydol oes, digidol a meddwl dwfn disgyblion, bydd gan yr ymgeisydd llwyddiannus y cyfle i ysgogi, i ysbrydoli ac i arfogi ein disgyblion â’r medrau hanfodol ar gyfer dyfodol llwyddiannus.</p> 
+
+<p>Wedi ei leoli ar gyrion Dinas Abertawe ac yn gyfleus iawn ar gyfer yr M4, ers agor yn 2003, mae Bryn Tawe wedi ennill bri fel ysgol gartrefol, lwyddiannus ac uchelgeisiol ar gyfer eu holl ddisgyblion. Mae staff gweithgar a thalentog yr ysgol yn allweddol wrth yrru llwyddiannau’r ysgol yn ogystal â sicrhau’r ymdeimlad cryf o gymuned sydd yn bodoli yma.</p> 
+
 <p>Rydym yn gallu cynnig:</p>
+
 <ul>
-	<li>y cyfle i weithio gyda disgyblion brwdfrydig, hapus a gweithgar</li>
-	<li>y cyfle i weithio o fewn Adran sydd yn ymrwymedig i gydweithio’n agos er mwyn sicrhau llwyddiant pob disgybl o bob gallu.</li>
-	<li>y cyfle i weithio gyda thîm o athrawon a staff cefnogol proffesiynol, talentog a blaengar, sydd yn llwyr ymrwymedig i sicrhau llwyddiant yr ysgol ac yn barhaus yn barod i fynd yr ail filltir er lles ein disgyblion.</li>
-	<li>y potensial ar gyfer datblygiad gyrfa pellach o fewn y Gyfadran a’r ysgol.</li>
+	<li>y cyfle i weithio gyda disgyblion brwdfrydig, hapus a gweithgar;</li>
+	<li>y cyfle i weithio o fewn dwy Adran sydd yn ymrwymedig i gydweithio’n agos er mwyn sicrhau llwyddiant pob disgybl o bob gallu;</li>
+	<li>y cyfle i weithio gyda thîm o athrawon a staff cefnogol proffesiynol,  talentog a blaengar, sydd yn llwyr ymrwymedig i sicrhau llwyddiant yr ysgol ac yn barhaus yn barod i fynd yr ail filltir er lles ein disgyblion.</li>
+	<li>y potensial ar gyfer datblygiad gyrfa pellach o fewn yr Adran a’r ysgol.</li>
 </ul>
+
 <ul class="list-unstyled">
-	<li><strong>Dyddiad Cau</strong>: Dydd Llun y 3ydd o Chwefror 2020 12:00</li>
-	<li><strong>Cyfweliadau</strong>: Dydd Gwener y 7fed o Chwefror 2020</li>
+	<li><strong>Dyddiad Cau</strong>: Dydd Iau 26ain o Fawrth 2020 am 12:00y.h.</li>
+	<li><strong>Cyfweliadau</strong>: yr wythnos yn dechrau ar yr 30ain o Fawrth 2020</li>
 	<li><strong>Graddfa cyflog</strong>: Prif raddfa gyflog</li>
-	<li><strong>Telerau’r swydd</strong>: Llawn amser. Cytundeb parhaol i ymgeisydd sydd a’r medrau a’r profiad addas. Ystyrir ceisiadau i weithio yn rhan amser.</li>
-	<li><strong>Dyddiad dechrau</strong>: yr 20fed o Ebrill 2020, neu y 1af o Fedi 2020 - yn unol ac argaeledd yr ymgeisydd llwyddiannus.</li>
+	<li><strong>Telerau’r swydd</strong>: cytundeb llawn amser a pharhaol</li>
 </ul>
+
+<p>Am ffurflen gais neu fanylion pellach, cysylltwch â Mrs Eirian Leonard, Rheolwraig Busnes yr ysgol ar y rhifau isod neu drwy e-bost:
+	LeonardE18@hwbcymru.net</p>
+
+<p><strong>Bydd angen dychwelyd y ffurflen gais drwy e-bost yn uniongyrchol at Mrs Eirian Leonard, Rheolwraig Busnes yr ysgol erbyn y dyddiad cau uchod.</strong> </p> 
 
 <p><a href="vacancies/Teacher-June18-Welsh.pdf">Lawrlwythwch ffurflen gais</a></p>
 
 <hr>
+<h3>Athro Addysg Gorfforol</h3>
 
-<h3>Hysbyseb Athro / Arweinydd Ffiseg</h3>
-<p>Mae corff Llywodraethu yr ysgol am benodi Arweinydd yn yr adran Ffiseg. Rydym yn chwilio am berson sydd â hanes o ardderchogrwydd o ran addysgu, yn gallu ysbrydoli disgyblion a chydweithwyr ac yn barod i ddatblygu strategaethau blaengar i symud yr adran lwyddiannus yma yn ei blaen eto.</p>
-<p>Ers agor yn 2003, mae Bryn Tawe wedi ennill bri fel ysgol gartrefol, lwyddiannus ac uchelgeisiol ar gyfer eu holl ddisgyblion wedi ei leoli ar gyrion Dinas Abertawe, yn gyfleus iawn ar gyfer yr M4. Mae staff gweithgar a thalentog yr ysgol yn allweddol wrth yrru llwyddiannau’r ysgol yn ogystal â sicrhau’r ymdeimlad cryf o gymuned sydd yn bodoli yma.</p>
-<p>Rydym yn chwilio am athro/athrawes graddedig gyda’r brwdfrydedd, profiad, egni a’r dychymyg i barhau i ddatblygu’r gwaith da a gyflawnwyd eisoes yn yr adran Ffiseg. Yn benodol, rydym yn chwilio am unigolyn sydd â’r weledigaeth a’r sgiliau i allu arwain a datblygu Ffiseg ymhellach yng NghA5 yn yr ysgol. Yn ogystal â’r cyfrifoldeb ychwanegol yma, ceir cyfle i addysgu dosbarthiadau ar draws yr ystod oed a gallu ac mae pwyslais cryf ar gynnig gwaith ymarferol i’n disgyblion sydd yn dod a’r pwnc yn fyw!</p>
-<p>Mae gan yr adran Wyddoniaeth saith labordy gyda’r cyfarpar diweddaraf. Mae’r Gwyddorau’n ddewisiadau poblogaidd ar gyfer Safon Uwch ac rydym yn cynnig Gwyddoniaeth Driphlyg ar lefel TGAU. Cymraeg yw iaith addysgu’r adran Wyddoniaeth ac fe fydd disgwyl i’r ymgeisydd llwyddiannus addysgu’r pwnc ar draws pob cyfnod allweddol a chynnal ac ymestyn ystod o weithgareddau allgyrsiol o fewn cyfadran gynhaliol a blaengar.</p>
+<p>Rydym yn chwilio am athro ysbrydoledig ac uchelgeisiol i ymuno ag adran lwyddiannus. Rydym yn chwilio am athro i ddarparu profiadau cyfoethog Addysg Gorfforol i ferched yr ysgol yn bennaf. Bydd cyfle i’r ymgeisydd llwyddiannus i addysgu ar draws pob cyfnod allweddol, gan gyfrannu at gyrsiau TGAU a Safon Uwch Addysg Gorfforol a chyrsiau Chwaraeon galwedigaethol.</p>
+
+<p>Gyda datblygiadau cyffrous iawn ym myd addysg, â’r ffocws ar ddatblygu medrau llythrennedd, rhifedd, digidol a meddwl dwfn disgyblion, bydd gan yr ymgeisydd llwyddiannus y cyfle i ysgogi, i ysbrydoli ac i arfogi ein disgyblion â’r medrau hanfodol ar gyfer sicrhau dyfodol llwyddiannus.</p>
+
+<p>Wedi ei leoli ar gyrion Dinas Abertawe ac yn gyfleus iawn ar gyfer yr M4, ers agor yn 2003, mae Bryn Tawe wedi ennill bri fel ysgol gartrefol, lwyddiannus ac uchelgeisiol ar gyfer eu holl ddisgyblion. Mae staff gweithgar a thalentog yr ysgol yn allweddol wrth yrru llwyddiannau’r ysgol yn ogystal â sicrhau’r ymdeimlad cryf o gymuned sydd yn bodoli yma.</p> 
+
 <p>Rydym yn gallu cynnig:</p>
-<ul>
-	<li>y cyfle i weithio gyda disgyblion brwdfrydig, hapus a gweithgar</li>
-	<li>y cyfle i weithio o fewn adran Wyddoniaeth sydd yn ymrwymedig i gydweithio’n agos er mwyn sicrhau llwyddiant pob disgybl o bob gallu.</li>
-	<li>y cyfle i weithio gyda tîm o athrawon a staff talentog, proffesiynol a blaengar, sydd yn llwyr ymrwymedig i sicrhau llwyddiant yr ysgol ac yn barhaus yn barod i fynd yr ail filltir er lles ein disgyblion.</li>
-	<li>y cyfle i ddysgu’n broffesiynol mewn ysgol gartrefol ble mae cefnogaeth i’r holl staff yn allweddol.</li>
-	<li>y potensial ar gyfer datblygiad gyrfa pellach o fewn a thu allan i’r adran a’r ysgol.</li>
-</ul>
-<p>Os ydych chi’n berson sydd yn meddu ar y rhinweddau rydym ni yn chwilio amdanynt, byddem yn falch iawn i glywed gennych. Mae croeso i chi gysylltu â rheolwr busnes yr ysgol, Mrs Eirian Leonard am fanylion pellach.</p>
-<ul class="list-unstyled">
-	<li><strong>Dyddiad Cau</strong>: Dydd Gwener 17eg o Ionawr 12:00</li>
-	<li><strong>Rhestr fer</strong>: Dydd Gwener 17eg o Ionawr</li>
-	<li><strong>Cyfweliadau</strong>: Wythnos yn dechrau 20fed o Ionawr 2020</li>
-	<li><strong>Graddfa cyflog</strong>: Prif raddfa; CDA 2A ar gyfer arwain CA5 Ffiseg i ymgeisydd addas</li>
-</ul>
-<p><a href="vacancies/Teacher-June18-Welsh.pdf">Lawrlwythwch ffurflen gais</a></p>
 
-<hr>
+    <ul>
+    	<li>y cyfle i weithio gyda disgyblion brwdfrydig, hapus a gweithgar.</li>
+    	<li>y cyfle i weithio o fewn Adran Addysg Gorfforol a MDaPh Iechyd a Lles, sydd yn ymrwymedig i ddatblygu creadigrwydd addysgu a chydweithio’n agos er mwyn sicrhau llwyddiant a lles pob disgybl o bob gallu.</li>
+    	<li>y cyfle i weithio gyda thîm o athrawon a staff cefnogol, proffesiynol, talentog a blaengar, sydd yn llwyr ymrwymedig i sicrhau llwyddiant yr ysgol ac yn barhaus yn barod i fynd yr ail filltir er lles ein disgyblion.</li>
+    	<li>y potensial ar gyfer datblygiad gyrfa pellach o fewn yr adran a’r ysgol.</li>
+    </ul>
 
-<h3>Athro Y Gymraeg / Celfyddydau Mynegiannol</h3>
-<p>Mae corff Llywodraethu yr ysgol am benodi Athro Y Gymraeg a Chelfyddydau Mynegiannol. Rydym yn chwilio am berson sydd â hanes o ardderchogrwydd o ran addysgu, yn gallu ysbrydoli disgyblion a chydweithwyr ac yn barod i ddatblygu strategaethau blaengar i symud yr adran lwyddiannus yma yn ei blaen eto.</p>
-<p>Ers agor yn 2003, mae Bryn Tawe wedi ennill bri fel ysgol gartrefol, lwyddiannus ac uchelgeisiol ar gyfer eu holl ddisgyblion wedi ei leoli ar gyrion Dinas Abertawe, yn gyfleus iawn ar gyfer yr M4. Mae staff gweithgar a thalentog yr ysgol yn allweddol wrth yrru llwyddiannau’r ysgol yn ogystal â sicrhau’r ymdeimlad cryf o gymuned sydd yn bodoli yma.</p>
-<p>Rydym yn chwilio am athro/athrawes ysbrydoledig ac uchelgeisiol i ymuno ag adran lwyddiannus iawn yn yr ysgol. Bydd disgwyl i’r ymgeisydd llwyddiannus i addysgu Y Gymraeg a chyfrannu at wersi Celfyddydau Mynegiannol yn ôl arbenigedd, gan gyfrannu at gyrsiau TGAU a Safon Uwch.</p>
-<p>Gyda datblygiadau cyffrous iawn ym myd addysg, â’r ffocws ar ddatblygu medrau llythrenned, rhifedd, digidol a meddwl dwfn disgyblion, bydd gan yr ymgeisydd llwyddiannus y cyfle i ysgogi, i ysbrydoli ac i arfogi ein disgyblion â’r medrau hanfodol ar gyfer dyfodol llwyddiannus.</p>
-<p>Rydym yn gallu cynnig:</p>
-<ul>
-	<li>y cyfle i weithio gyda disgyblion brwdfrydig, hapus a gweithgar</li>
-	<li>y cyfle i weithio o fewn Adran sydd yn ymrwymedig i gydweithio’n agos er mwyn sicrhau llwyddiant pob disgybl o bob gallu.</li>
-	<li>y cyfle i weithio gyda thîm o athrawon a staff cefnogol proffesiynol, talentog a blaengar, sydd yn llwyr ymrwymedig i sicrhau llwyddiant yr ysgol ac yn barhaus yn barod i fynd yr ail filltir er lles ein disgyblion.</li>
-	<li>y potensial ar gyfer datblygiad gyrfa pellach o fewn y Gyfadran a’r ysgol.</li>
-</ul>
 <ul class="list-unstyled">
-	<li><strong>Dyddiad Cau</strong>: Dydd Gwener 24ain o Ionawr am 12:00</li>
-	<li><strong>Rhestr fer</strong>: Dydd Gwener 24ain o Ionawr</li>
-	<li><strong>Cyfweliadau</strong>: wythnos yn dechrau 27ain o Ionawr 2020</li>
+	<li><strong>Dyddiad Cau</strong>: Dydd Iau 26ain o Fawrth 2020 am 12:00y.h.</li>
+	<li><strong>Cyfweliadau</strong>: yr wythnos yn dechrau ar yr 30ain o Fawrth 2020</li>
 	<li><strong>Graddfa cyflog</strong>: Prif raddfa gyflog</li>
-	<li><strong>Telerau’r swydd</strong>: Llawn amser.</li>
+	<li><strong>Telerau’r swydd</strong>: cytundeb llawn amser a pharhaol (ystyrir geisiadau rhan amser i ymgeiswyr addas)</li>
 </ul>
 
-<p><a href="vacancies/Teacher-June18-Welsh.pdf">Lawrlwythwch ffurflen gais</a></p>
+<p>Am ffurflen gais neu fanylion pellach, cysylltwch â Mrs Eirian Leonard, Rheolwraig Busnes yr ysgol ar y rhifau isod neu drwy e-bost:
+	LeonardE18@hwbcymru.net</p>
 
+<p><strong>Bydd angen dychwelyd y ffurflen gais drwy e-bost yn uniongyrchol at Mrs Eirian Leonard, Rheolwraig Busnes yr ysgol erbyn y dyddiad cau uchod.</strong></p>  
+
+<p><a href="vacancies/Teacher-June18-Welsh.pdf">Lawrlwythwch ffurflen gais</a></p>
 
 <!-- 
 Support staff appplication form
